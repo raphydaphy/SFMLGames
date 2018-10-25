244c2a3e3952482c1d1c3265462e4f00beecd599
Raph Hennessy <raph.hennessy@gmail.com>
2018-10-25 17:18:29 +1100

    Working SFML !

---
+++ SFMLGames/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Snake");
+
+	sf::CircleShape shape(300.f, 10000);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
