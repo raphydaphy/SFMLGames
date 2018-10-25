dc116addbf201c86d9ecf8ac364d63d3e41cd06f
Raph Hennessy <raph.hennessy@gmail.com>
2018-10-25 16:16:24 +1100

    Works with MinGW

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 
 int main()
 {
+    sf::Window window;
+    window.create(sf::VideoMode(800, 600), "SFML Games");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
